5bc1a7a0a982973fe170099a62fd29b21baaf6f5
yamashita <hirotsugu_yamashita@dwango.co.jp>
2017-01-30 23:49:03 +0900

    fix test

--- spec/statSpec.js
+++ spec/statSpec.js
@@ -123,11 +123,11 @@ describe("format stat result", () => {
 		"aac audio: 4.16KB\n" +
 		"script: 494B (6%)\n" +
 		"other: 367B (4%)\n" +
-		"[deprecated] AAC is deprecated. Use MP4 instead.\n" +
 		"  game.json: 367B\n" +
 		"[*] TOTAL SIZE (using ogg): 8.43KB (8632B)\n" +
 		"[ ] TOTAL SIZE (using mp4): 6.29KB (6443B)\n" +
-		"[ ] TOTAL SIZE (using aac): 5.14KB (5267B)\n";
+		"[ ] TOTAL SIZE (using aac): 5.14KB (5267B)\n" +
+		"WARN: AAC is deprecated. Use MP4 instead.\n";
 
 	it("will output following text", done => {
 		let buffer = "";
@@ -152,11 +152,11 @@ describe("format stat result - maximum mp4", () => {
 		"aac audio: 4.16KB\n" +
 		"script: 0B (0%)\n" +
 		"other: 168B (2%)\n" +
-		"[deprecated] AAC is deprecated. Use MP4 instead.\n" +
 		"  game.json: 168B\n" +
 		"[ ] TOTAL SIZE (using ogg): 7.61KB (7795B)\n" +
 		"[*] TOTAL SIZE (using mp4): 9.48KB (9704B)\n" +
-		"[ ] TOTAL SIZE (using aac): 4.33KB (4430B)\n";
+		"[ ] TOTAL SIZE (using aac): 4.33KB (4430B)\n" +
+		"WARN: AAC is deprecated. Use MP4 instead.\n";
 
 	it("will output following text", done => {
 		let buffer = "";
